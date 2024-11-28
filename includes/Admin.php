4f104c0a89003c9b09fd8d5bab4c6fd6a815ed07
BrainStorm <brainstorm@programist.ru>
2024-11-28 20:29:54 +0300

    Plugin dashboard page

--- includes/Admin.php
+++ includes/Admin.php
@@ -12,10 +12,10 @@ class Admin
     public function add_admin_menu()
     {
         add_menu_page(
-            'My Class Plugin',
-            'My Plugin',
+            'B8 WP CF7 Telegram',
+            'B8 WP CF7 Telegram',
             'manage_options',
-            'my-class-plugin',
+            'b8-wp-cf7-telegram-settings',
             [$this, 'render_admin_page'],
             'dashicons-admin-generic',
             6
@@ -24,6 +24,24 @@ class Admin
 
     public function render_admin_page()
     {
-        echo '<div class="wrap"><h1>My Class Plugin Settings</h1></div>';
+        $output = '';
+        $output .= '<div class="wrap">';
+        $output .= '<h1>' . _('WP CF7 Telegram Settings', 'b8-wp-cf7-telegram') . '</h1>';
+
+        if (class_exists('WPCF7_ContactForm')) {
+            // Получаем все формы
+            $forms = WPCF7_ContactForm::find();
+
+            // Перебираем формы
+            foreach ($forms as $form) {
+                $output .= 'ID: ' . $form->id() . '<br>';
+                $output .= 'Название: ' . $form->title() . '<br>';
+                $output .= 'Краткий код: [contact-form-7 id="' . $form->id() . '" title="' . $form->title() . '"]<br><hr>';
+            }
+        } else {
+            echo 'Contact Form 7 не активен.';
+        }
+        $output .= '</div>';
+        echo $output;
     }
 }
